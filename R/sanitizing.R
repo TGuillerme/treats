c5a710d55b4889125af92bc719e7f349af4afa52
TGuillerme <guillert@tcd.ie>
2020-10-15 12:28:48 +0100

    Sanitizing functions (imported from dispRity)

---
+++ R/sanitizing.R
@@ -0,0 +1,132 @@
+## SANITIZING FUNCTIONS
+## Checking the class of an object and returning an error message if != class
+check.class <- function(object, class, msg, errorif = FALSE) {
+    ## Get call
+    match_call <- match.call()
+
+    ## class_object variable initialisation
+    class_object <- class(object)[1]
+    ## class_length variable initialisation
+    length_class <- length(class)
+
+    ## Set msg if missing
+    if(missing(msg)) {
+        if(length_class != 1) {
+            msg <- paste(" must be of class ", paste(class, collapse = " or "), ".", sep = "")
+        } else {
+            msg <- paste(" must be of class ", class, ".", sep = "")
+        }
+    }
+
+    ## check if object is class.
+    if(length_class != 1) {
+    ## check if object is class in a cascade (class[1] else class[2] else class[3], etc..)
+    ## returns error only if object is not of any class
+
+        error <- NULL
+        for(counter in 1:length_class) {
+            if(errorif != TRUE) {
+                if(class_object != class[counter]) {
+                    error <- c(error, TRUE)
+                } else {
+                    error <- c(error, FALSE)
+                }
+            } else {
+                if(class_object == class[counter]) {
+                    error <- c(error, TRUE)
+                } else {
+                    error <- c(error, FALSE)
+                }
+            }
+        }
+        ## If function did not return, class is not matching
+        if(!any(!error)) {
+            stop(match_call$object, msg, call. = FALSE)
+        } else {
+            return(class_object)
+        }
+
+    } else {
+        if(errorif != TRUE) {
+            if(class_object != class) {
+                stop(match_call$object, msg , call. = FALSE)
+            }
+        } else {
+            if(class_object == class) {
+                stop(match_call$object, msg , call. = FALSE)
+            }        
+        }
+    } 
+}
+
+## Checking the class of an object and returning an error message if != class
+check.length <- function(object, length, msg, errorif = FALSE) {
+
+    match_call <- match.call()
+
+    if(errorif != TRUE) {
+        if(length(object) != length) {
+            stop(match_call$object, msg , call. = FALSE)
+        }
+    } else {
+        if(length(object) == length) {
+            stop(match_call$object, msg , call. = FALSE)
+        }        
+    }
+}
+
+## Checking if a method matches the authorized method
+check.method <- function(argument, all_arguments, msg, condition = all) {
+    if(condition(is.na(match(argument, all_arguments)))) {
+        stop(paste(msg, " must be one of the following: ", paste(all_arguments, collapse = ", "), ".", sep = ""), call. = FALSE)
+    }
+}
+
+## Checking if a matrix is a distance matrix
+check.dist.matrix <- function(matrix, method, just.check = FALSE, ...) {
+
+    ## Is distance
+    was_dist <- FALSE
+
+    ## Check if distance
+    if(is(matrix, "dist")) {
+        return(list(matrix, "was_dist" = TRUE))
+    }
+
+    ## Is the matrix square?
+    if(dim(matrix)[1] == dim(matrix)[2] &&
+       all(diag(as.matrix(matrix)) == 0) &&
+       all(matrix[upper.tri(matrix)] == matrix[rev(lower.tri(matrix))])) {
+        ## It was a distance matrix!
+        was_dist <- TRUE
+    }
+
+    if(just.check) {
+        ## Simply return the check
+        return(was_dist)
+    } else {
+        ## Return a matrix
+        if(was_dist) {
+            return(list(stats::as.dist(matrix), "was_dist" = TRUE))
+        } else {
+            return(list(vegan::vegdist(matrix, method = method, ...), "was_dist" = FALSE))
+        }
+    }
+}
+
+## Stop with call message wrapper function
+stop.call <- function(call, msg, msg.pre = "") {
+    stop(paste0(msg.pre, as.expression(call), msg), call. = FALSE)
+}
+
+## Check through a list
+check.list <- function(list, check.fun, condition, ...) {
+    ## Run the checks
+    check_results <- lapply(list, check.fun, ...)
+    ## Apply the condition
+    if(!missing(condition)) {
+        return(unlist(lapply(check_results, condition)))
+    } else {
+        return(unlist(check_results))
+    }
+}
---
+++ tests/testthat/test-sanitizing.R
@@ -0,0 +1,170 @@
+## TEST sanitizing
+
+context("sanitizing")
+
+## Testing class.check
+## examples
+class_1<-NA ; class(class_1) <- 'class_1'
+class_2<-NA ; class(class_2) <- 'class_2'
+class_3<-NA ; class(class_3) <- 'class_3'
+class_list<-c("class_1", "class_2")
+
+## Test
+test_that("check.class works", {
+    ## class - single
+    expect_null(
+            check.class(class_1, "class_1", 'test')
+            )
+    expect_error(
+            check.class(class_1, "class_1", 'test', errorif=TRUE)
+            )
+    expect_null(
+            check.class(class_2, "class_2", 'test')
+            )
+    expect_error(
+            check.class(class_2, "class_2", 'test', errorif=TRUE)
+            )
+    ## class - multiple
+    expect_that(
+            check.class(class_1, class_list, 'test'), equals("class_1")
+            )
+    expect_that(
+            check.class(class_2, class_list, 'test'), equals("class_2")
+            )
+    expect_error(
+            check.class(class_3, class_list, 'test')
+            )
+
+    expect_that(
+            check.class(class_2, class_list, 'test', errorif = TRUE), equals("class_2"))
+})
+
+## Check class function
+test_that("check.class works with functions", {
+    object_fun <- function(x) return(x+1)
+    object_std <- mean
+
+    ## Normal behavior
+    expect_is(
+        object_fun
+        ,"function")
+    expect_null(
+        check.class(object_fun, "function")
+    )
+    expect_null(
+        check.class(object_std, "function")
+    )
+    expect_error(
+        check.class(object_std, "numeric")
+    )
+    expect_error(
+        check.class(object_fun, "standardGeneric")
+    )
+})
+
+## Testing check.length
+## examples
+length_1 <- 1
+length_2 <- c(1, 1)
+length_3 <- NA
+length_4 <- "1"
+
+## Test
+test_that("check.length works", {
+    expect_null(
+            check.length(length_1, '1', 'test')
+            )
+    expect_null(
+            check.length(length_3, '1', 'test')
+            )
+    expect_null(
+            check.length(length_4, '1', 'test')
+            )
+    expect_error(
+            check.length(length_2, '1', 'test')
+            )
+    expect_error(
+            check.length(length_1, '1', 'test', errorif=TRUE)
+            )
+})
+
+## Test methods
+test_that("check.method works", {
+    methods <- letters[1:2]
+    expect_null(check.method("a", methods))
+    expect_null(check.method("b", methods))
+    expect_null(check.method(c("a", "b"), methods))
+    expect_null(check.method(c("b", "a"), methods))
+    expect_error(check.method("c", methods))
+    expect_null(check.method(c("a", "c"), methods))
+})
+
+## Test distance
+test_that("check.dist.matrix works", {
+    non_dist <- matrix(1:9, 3, 3)
+    is_dist <- as.matrix(dist(non_dist))
+
+    test <- check.dist.matrix(dist(non_dist))
+    expect_equal(test[[1]], dist(non_dist))
+    expect_true(test$was_dist)
+
+    expect_error(check.dist.matrix(non_dist, just.check = "blabla"))
+    expect_error(check.dist.matrix(non_dist, just.check = FALSE))
+    expect_false(check.dist.matrix(non_dist, just.check = TRUE))
+    expect_true(check.dist.matrix(is_dist, just.check = TRUE))
+    test <- check.dist.matrix(non_dist, just.check = FALSE, method = "euclidean")
+    expect_equal(class(test), "list")
+    expect_equal(class(test[[1]]), "dist")
+    expect_false(test[[2]])
+    test <- check.dist.matrix(is_dist, just.check = FALSE, method = "euclidean")
+    expect_equal(class(test), "list")
+    expect_equal(class(test[[1]]), "dist")
+    expect_true(test[[2]])
+    expect_true(all(test[[1]] == dist(non_dist)))
+})
+
+## Stop call
+test_that("stop.call works", {
+
+    return.match.call <- function(character, numeric, fun, formula, matrix) {
+        match_call <- match.call()
+        return(match_call)
+    }
+
+    my_matrix <- matrix(NA)
+    call <- return.match.call("a", 1, mean, ~ a + b, my_matrix)
+
+    test <- capture_error(stop.call(call$character, " works."))
+    expect_equal(test[[1]], "a works.")
+    test <- capture_error(stop.call(call$numeric, " works."))
+    expect_equal(test[[1]], "1 works.")
+    test <- capture_error(stop.call(call$fun, " works."))
+    expect_equal(test[[1]], "mean works.")
+    test <- capture_error(stop.call(call$formula, " works."))
+    expect_equal(test[[1]], "~a + b works.")
+    test <- capture_error(stop.call(call$matrix, " works."))
+    expect_equal(test[[1]], "my_matrix works.")
+
+    test <- capture_error(stop.call(call$character, " works.", "look: "))
+    expect_equal(test[[1]], "look: a works.")
+})
+
+## Check class
+test_that("check.class works", {
+    list <- list("a" = "a", "1" = 1, "tree" = rtree(5))
+    test <- check.list(list, function(x) !is.null(x))
+    expect_equal(test, c("a" = TRUE, "1" = TRUE, "tree" = TRUE))
+
+    test <- check.list(list, is.null, condition = any)
+    expect_equal(test, c("a" = FALSE, "1" = FALSE, "tree" = FALSE))
+
+    test <- check.list(list, function(x) (x == "a"), condition = any)
+    expect_equal(test, c("a" = TRUE, "1" = FALSE, "tree" = FALSE))    
+})
+
+## Test round
+test_that("test_equal_round works", {
+    x <- 1.111111
+    y <- 1.11
+    expect_equal(expect_equal_round(x, y, digits = 2), 1.11)
+})
