376fa3b0d012d3a616e45bef51e6a1c51b3b1b34
TGuillerme <guillert@tcd.ie>
2020-10-19 17:11:11 +0100

    trait.process properly handled
    TODO: remove sim.motion at some point!

--- R/sim.motion.R
+++ R/sim.motion.R
@@ -1,89 +1,38 @@
-## A generalised function for simulating a motion
-#' @param process the motion function (e.g. a bm)
-#' @param steps the length of the simulation
-#' @param parameters optional, parameters for process
-sim.motion <- function(process, steps, initial.value = 0, parameters = list(var = 1)) {
-    count <- 0
-    parameters$x0 <- initial.value
-    output <- do.call(process, parameters)
+# ## A generalised function for simulating a motion
+# #' @param process the motion function (e.g. a bm)
+# #' @param steps the length of the simulation
+# #' @param parameters optional, parameters for process
+# sim.motion <- function(process, steps, initial.value = 0, parameters = list(var = 1)) {
+#     count <- 0
+#     parameters$x0 <- initial.value
+#     output <- do.call(process, parameters)
     
-    while(count < (steps-1)) {
-        count <- count + 1
-        parameters$x0 <- output[1]
-        output <- c(do.call(process, parameters), output)
-    }
-    return(rev(output))
-}
+#     while(count < (steps-1)) {
+#         count <- count + 1
+#         parameters$x0 <- output[1]
+#         output <- c(do.call(process, parameters), output)
+#     }
+#     return(rev(output))
+# }
 
+# # plot.simulation(main = "Normal",
+# #                     replicate(500,
+# #                         sim.motion(process = step.no.process, 
+# #                                    parameters = list(fun = rnorm, options = list(mean = 0, sd = 1)),
+# #                                    steps = 100)))
+# # plot.simulation(main = "LogNormal",
+# #                     replicate(500,
+# #                         sim.motion(process = step.no.process, 
+# #                                    parameters = list(fun = rlnorm, options = list(meanlog = 0, sdlog = 1)),
+# #                                    steps = 100)))
+# # plot.simulation(main = "Gamma",
+# #                     replicate(500,
+# #                         sim.motion(process = step.no.process, 
+# #                                    parameters = list(fun = rgamma, options = list(shape = 1)),
+# #                                    steps = 100)))
 
-
-## Non process function
-#' @param x0 ignored
-#' @param fun
-#' @param options
-step.no.process <- function(x0, fun, options) {
-    options$n <- 1
-    do.call(fun, options)
-}
-
-plot.simulation(main = "Normal",
-                    replicate(500,
-                        sim.motion(process = step.no.process, 
-                                   parameters = list(fun = rnorm, options = list(mean = 0, sd = 1)),
-                                   steps = 100)))
-# plot.simulation(main = "LogNormal",
-#                     replicate(500,
-#                         sim.motion(process = step.no.process, 
-#                                    parameters = list(fun = rlnorm, options = list(meanlog = 0, sdlog = 1)),
-#                                    steps = 100)))
-# plot.simulation(main = "Gamma",
-#                     replicate(500,
-#                         sim.motion(process = step.no.process, 
-#                                    parameters = list(fun = rgamma, options = list(shape = 1)),
-#                                    steps = 100)))
-
-# plot.simulation(main = "Uniform",
-#                     replicate(500,
-#                         sim.motion(process = step.no.process, 
-#                                    parameters = list(fun = runif, options = list(max = 1, min = -1)),
-#                                    steps = 100)))
-
-
-## BM motion
-#' @param x0
-#' @param sd
-#' @param n
-step.BM <- function(x0, edge.length = 1, sd = 1, ...) {
-    ## Multidimensional traits
-    return(t(MASS::mvrnorm(n = 1, mu = x0, Sigma = sd * edge.length)))
-}
-
-plot.simulation(main = "Uniform",
-                    replicate(500,
-                        sim.motion(process = step.BM, 
-                                   parameters = list(sd = 1),
-                                   steps = 100)))
-
-
-
-
-
-## OU motion
-#' @param x0
-#' @param var
-#' @param alpha
-#' @param theta
-step.OU <- function(x0, var, alpha, theta = 0) {
-    mean <- theta + (x0 - theta) * exp(-alpha)
-    sd   <- sqrt(var/(2 * alpha) * (1 - exp(-2 * alpha)))
-    return(rnorm(1, mean = mean, sd = sd))
-}
-
-# plot.simulation(main = "OU (alpha = 0.05)",
-#                     replicate(500,
-#                         sim.motion(process = step.OU, 
-#                                    parameters = list(var = 1, alpha = 0.05),
-#                                    steps = 100)))
-
-
-# stop("For multivariate estimation use: MASS::mvrnorm()? Or compare to space.maker approach")
+# # plot.simulation(main = "Uniform",
+# #                     replicate(500,
+# #                         sim.motion(process = step.no.process, 
+# #                                    parameters = list(fun = runif, options = list(max = 1, min = -1)),
+# #                                    steps = 100)))
---
+++ R/trait.process.R
@@ -0,0 +1,90 @@
+#' @name trait.process
+#' @aliases BM.process OU.process
+#' @title Trait processes
+#'
+#' @description Different trait processes implemented in dads.
+#'
+#' @usage trait.process(x0, edge.length, ...)
+#'
+#' @param x0 The previous state. This can be a single value (unidimensional process) or more (multidimensional processes).
+#' @param edge.length The branch length (default must be 1). This is always a single value.
+#' @param ... Any optional argument for the specific process (see details).
+#' 
+#' @details
+#' The different trait processes implemented in dads are:
+#' 
+#' \itemize{
+#'      \item{BM.process} A Brownian motion process (uni or multidimensional). This function is based on \code{\link[MASS]{mvrnorm}}.
+#'      This process can take following optional arguments:
+#'          \itemize{
+#'               \item \code{Sigma} a positive-definite symmetric matrix specifying the covariance matrix of the variables (default is \code{diag(length(x0))}).
+#'               \item \code{...} any named additional argument to be passed to \code{\link[MASS]{mvrnorm}}.
+#'          }
+#' 
+#'      \item{OU.process} A Ornstein-Uhlenbeck process (uni or multidimensional). This function is based on \code{\link[MASS]{mvrnorm}}.
+#'      This process can take following optional arguments:
+#'          \itemize{
+#'               \item \code{var} the process overall variance (default = is \code{1}).
+#'               \item \code{alpha} the alpha parameter (default = is \code{1}).
+#'               \item \code{theta} the theta parameter (default = is \code{0}).
+#'               \item \code{...} any named additional argument to be passed to \code{\link[MASS]{mvrnorm}}.
+#'          }
+#'      
+#'      \item{no.process} An non-process unidimensional function. This function generates a trait value not depending on the branch length nor the previous state
+#'      This process can take following optional arguments:
+#'          \itemize{
+#'               \item \code{fun} a random number function (default is \code{\link[stats]{rnorm}}).
+#'               \item \code{...} any named additional argument to be passed to \code{fun}.
+#'          }
+#' }
+#' 
+#' @examples
+#' ## NOTE: You can visualise most process by making them
+#' ## into a "dads" "traits" object using make.traits():
+#'
+#' ## The Brownian motion process
+#' BM.process(x0 = 0)
+#' plot(make.traits(process = BM.process))
+#' ## A correlation matrix between 3 traits
+#' cor_matrix <- matrix(c(1/3,1/3,1/3,1/3,2/3,0,1/3,0,2/3), ncol = 3)
+#' BM.process(x0 = c(0,0,0), Sigma = cor_matrix)
+#' 
+#' ## The Ornstein-Uhlenbeck process
+#' OU.process(x0 = 0)
+#' plot(make.traits(process = BM.process))
+#' 
+#' ## No process
+#' no.process()
+#' plot(make.traits(process = no.process))
+#' 
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+trait.process <- function(x0, edge.length = 1, ...) {
+    cat("Trait processes implemented in dads:\n")
+    cat("?BM.process\n")
+    cat("?OU.process\n")
+    cat("?no.process\n")
+}
+
+## The Brownian motion
+BM.process <- function(x0, edge.length = 1, Sigma = diag(length(x0)), ...) {
+    return(t(MASS::mvrnorm(n = 1, mu = x0, Sigma = Sigma * edge.length, ...)))
+}
+
+## The OU process
+OU.process <- function(x0, edge.length = 1, var = 1, alpha = 1, theta = 0, ...) {
+    ## Calculate the means
+    means <- theta + (x0 - theta) * exp(-alpha)
+    ## Calculate the Sigma
+    sd   <- sqrt(var/(2 * alpha) * (1 - exp(-2 * alpha)))
+    ## Get the traits
+    return(t(MASS::mvrnorm(n = 1, mu = means, Sigma = sd * edge.length, ...)))
+}
+
+## no.process
+no.process <- function(x0, edge.length = 1, fun = stats::rnorm, ...) {
+    return(fun(n = 1, ...))
+}
