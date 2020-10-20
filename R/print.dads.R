9ec64c567bd235bc4a629fbcaa23a04848891410
TGuillerme <guillert@tcd.ie>
2020-10-20 16:12:06 +0100

    print.dads INIT

---
+++ R/print.dads.R
@@ -0,0 +1,19 @@
+#' @title Prints a \code{dads} object.
+#'
+#' @description Summarises the content of a \code{dads} object.
+#'
+#' @param x A \code{dads} object.
+#' @param all \code{logical}; whether to display the entire object (\code{TRUE}) or just summarise its contents (\code{FALSE} - default).
+#' @param ... further arguments to be passed to \code{print} or to \code{print.dads}.
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+print.dads <- function(x, all = FALSE, ...) {
+
+    return()
+}
---
+++ tests/testthat/test-print.dads.R
@@ -0,0 +1,16 @@
+context("print.dads")
+
+## Test
+test_that("print.dads works", {
+
+    # ## Sanitizing
+    # expect_error(print.dads())
+
+    # ## Right output
+    # expect_is(
+    #     print.dads()
+    #     , "class")
+    # expect_equal(
+    #     dim(print.dads())
+    #     , dim)
+})
