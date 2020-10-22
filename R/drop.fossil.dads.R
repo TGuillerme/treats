f6ce786077f6c4946a83dddccaacbe45de7a27da
TGuillerme <guillert@tcd.ie>
2020-10-22 16:23:12 +0100

    drop.fossil.dads INIT

---
+++ R/drop.fossil.dads.R
@@ -0,0 +1,17 @@
+#' @title drop.fossil function from ape for dads objects
+#'
+#' @description 
+#'
+#' @param 
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+drop.fossil.dads <- function() {
+
+    return()
+}
