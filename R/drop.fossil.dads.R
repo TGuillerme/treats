8dc4ca4c73d7e7513bfc7aaeb1839de63ab5d9d9
TGuillerme <guillert@tcd.ie>
2020-10-23 16:23:17 +0100

    added drop fossil function

--- R/drop.fossil.dads.R
+++ R/drop.fossil.dads.R
@@ -1,8 +1,9 @@
-#' @title drop.fossil function from ape for dads objects
+#' @title Drop fossils
 #'
-#' @description 
+#' @description Remove fossils from \code{dads} objects or \code{phylo} objects (using \code{\link[ape]{drop.fossil}}).
 #'
-#' @param 
+#' @param phy \code{dads} data.
+#' @param tol a numeric value giving the tolerance to consider a species as extinct.
 #' 
 #' @examples
 #'
@@ -11,7 +12,16 @@
 #' @author Thomas Guillerme
 #' @export
 
-drop.fossil.dads <- function() {
+drop.fossil.dads <- function(phy, tol = 1e-8) {
 
-    return()
+    if(class(phy) == "phylo") {
+        ## Simply run drop.fossil
+        return(ape::drop.fossil(phy, tol = tol))
+    } else {
+        ## Drop fossils from the tree
+        phy$tree <- ape::drop.fossil(phy$tree, tol = tol)
+        ## Drop fossils from the data
+        phy$data <- phy$data[rownames(phy$data) %in% c(phy$tree$tip.label, phy$tree$node.label) , , drop = FALSE]
+        return(phy)
+    }
 }
---
+++ tests/testthat/test-drop.fossil.dads.R
@@ -0,0 +1,23 @@
+context("drop.fossils.dads")
+
+## Test
+test_that("drop.fossils.dads works", {
+
+    ## Create a simple bd tree
+    set.seed(1)
+    phy <- dads(bd.params = list(extinction = 1/3), stop.rule = list(max.living = 20))
+    expect_gt(Ntip(phy), 20)
+    test <- drop.fossil.dads(phy)
+    expect_is(test, "phylo")
+    expect_equal(Ntip(test), 20)
+
+    ## Create a simple bd tree + traits
+    set.seed(12)
+    phy <- dads(bd.params = list(extinction = 1/3), stop.rule = list(max.living = 20), traits = make.traits())
+    expect_gt(Ntip(phy$tree), 20)
+    expect_gt(nrow(phy$data), 39)
+    test <- drop.fossil.dads(phy)
+    expect_is(test, "dads")
+    expect_equal(Ntip(test$tree), 20)
+    expect_equal(nrow(test$data), 39)
+})
