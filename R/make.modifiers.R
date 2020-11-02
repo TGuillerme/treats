10f3a2227ba93534894b68bce6e4c0ad842cc4af
TGuillerme <guillert@tcd.ie>
2020-11-02 18:09:27 +0000

    Update make.modifiers.R

--- R/make.modifiers.R
+++ R/make.modifiers.R
@@ -2,25 +2,17 @@
 #'
 #' @description Making modifiers objects for dads based on an ancestor's (parent) trait.
 #'
-#' @param what       What to modify: can be either \code{"waiting"} or \code{"speciating"}.
-#' @param when       A logical function of how to apply the modification (see details).
-#' @param how        A function of what to return when the condition is met (see details).
-#' @param speciating A function for the speciating events (ignores all other arguments but \code{add} and \code{test}).
-#' @param waiting    A function for the waiting events (ignores all other arguments but \code{add} and \code{test})..
-#' @param add        Whether to this modifier to a \code{"dads"} \code{"modifier"} object
-#' @param test       Logical whether to test if the modifiers object will work (default is TRUE)
+#' @param branch.length A function for the waiting time generating branch length (see details).
+#' @param speciation    A function for triggering the speciation events (see details).
+#' @param condition     A function giving the condition on which to modify the output of \code{branch.length} or \code{speciation}. If missing the condition is always met (set to \code{condition = function(x, ...) TRUE})
+#' @param modify        A function giving the rule of how to modify the output of \code{branch.length} or \code{speciation}. If missing no modification is used (set to \code{modify = function(x, ...) x}).
+#' @param add           Whether to add this modifier to a \code{"dads"} \code{"modifier"} object.
+#' @param test          Logical whether to test if the modifiers object will work (default is TRUE),
+#' @param ...           Optional, any extra argument to be passed to the modifier function.
 #' 
 #' @details
 #' 
-#' \code{when} must be a function that inputs at least the parent trait value(s) as \code{x} and should return a logical value. For example, to use the condition "If the parent's first trait value is negative, do \code{how} (i.e. return \code{TRUE}):
-#' 
-#'     \code{when = function(x) return(x < 0)} 
-#' 
-#' \code{how} must be a function that inputs at least the parent trait value(s) as \code{x} and should return the value associated to \code{what} (i.e. a \code{numeric} value for \code{"waiting"} or a \code{logical} value for \code{"speciating"}. For example, to always allow speciation if \code{when} is \code{TRUE}:
-#' 
-#'      \code{how = function(x) return(TRUE)}
-#' 
-#' NOTE: modifiers only affect the birth death process after the first speciation event.
+#' \code{branch.length} must be a function that intakes the following arguments: 
 #' 
 #' @examples
 #'
@@ -29,97 +21,117 @@
 #' @author Thomas Guillerme
 #' @export
 
-make.modifiers <- function(what, when, how, speciating = NULL, waiting = NULL, add, test = TRUE) {
+make.modifiers <- function(branch.length, speciation, condition, modify, add, test = TRUE, ...) {
 
-    ## add
-    add_modifiers <- FALSE
-    if(!missing(add)) {
-        if(!(is(add, "dads") && is(add, "modifiers"))) {
-            stop("modifiers can only be added to objects of class dads and modifiers.")
+    ## required arguments
+    required_args <- c("bd.params", "n.taxa", "parent.lineage", "trait.values", "modify.fun", "...")
+
+    ## Check branch length
+    do_branch_length <- FALSE
+    if(!missing(branch.length)) {
+        check.class(branch.length, "function")
+        ## Check if it has the right arguments
+        check_args <- names(formals(branch.length))
+        if(any(missing <- is.na(match(required_args, branch_length_args)))) {
+            stop(paste0("The branch.length function is missing the following argument", ifelse(sum(missing) > 1, "s: ", ": "),  paste(required_args[missing], collapse = ", "  ), ". If ", ifelse(sum(missing) > 1, "they are", "it is"), " not required, you can set ", ifelse(sum(missing) > 1, "them", "it"), " to NULL."), call. = FALSE)
         }
-        add_modifiers <- TRUE
+        do_branch_length <- TRUE
+    }
+
+    ## Check speciation
+    do_speciation <- FALSE
+    if(!missing(speciation)) {
+        check.class(speciation, "function")
+        ## Check if it has the right arguments
+        check_args <- names(formals(speciation))
+        if(any(missing <- is.na(match(required_args, check_args)))) {
+            stop(paste0("The speciation function is missing the following argument", ifelse(sum(missing) > 1, "s: ", ": "),  paste(required_args[missing], collapse = ", "  ), ". If ", ifelse(sum(missing) > 1, "they are", "it is"), " not required, you can set ", ifelse(sum(missing) > 1, "them", "it"), " to NULL."), call. = FALSE)
+        }
+        do_speciation <- TRUE
+    }
+
+    ## Check condition
+    do_condition <- FALSE
+    if(!missing(condition)) {
+        check.class(condition, "function")
+        do_condition <- TRUE
+    }
+
+    ## Check modify
+    do_modify <- FALSE
+    if(!missing(modify)) {
+        check.class(modify, "function")
+        do_modify <- TRUE
     }
 
     ## test
     check.class(test, "logical")
+    
 
-    ## Speciating
-    if(!is.null(speciating)) {
-        ## Check the input
-        check.class(speciating, "function")
-        modifiers <- list("speciating" = speciating)
+    ## Set the defaults
+    default_branch_length <- !do_branch_length && !do_condition && !do_modify
+    default_speciation    <- !do_speciation    && !do_condition && !do_modify
 
-        ## Add to previous (if needed)
-        if(add_modifiers) {
-            add$speciating <- modifiers$speciating
-            modifiers <- add
+    ## add
+    add_modifiers <- FALSE
+    if(!missing(add)) {
+        ## Check input
+        if(!(is(add, "dads") && is(add, "modifiers"))) {
+            stop("modifiers can only be added to objects of class dads and modifiers.")
+        }
+        add_modifiers <- TRUE
+        ## Check what's done already
+        already_done <- names(add)
+
+        ## Check if waiting time existed
+        if("waiting" %in% names(add)) {
+            if(do_branch_length) {
+                warning("branch.length modifier overwritten.", call. = FALSE)
+            } else {
+                default_branch_length <- FALSE
+            } 
         }
 
-        ## Check object
-        if(test) {
-            check.modifiers(modifiers)
+        ## Check if speciating existed
+        if("speciating" %in% names(add)) {
+            if(do_branch_length) {
+                warning("speciation modifier overwritten.", call. = FALSE)
+            } else {
+                default_speciation <- FALSE
+            } 
         }
-        return(modifiers)
     }
 
-    ## Waiting
-    if(!is.null(waiting)) {
-        ## Check the input
-        check.class(waiting, "function")
-        modifiers <- list("waiting" = waiting)
-
-        ## Add to previous (if needed)
-        if(add_modifiers) {
-            add$waiting <- modifiers$waiting
-            modifiers <- add
+    ## Build the object
+    modifiers <- list()
+    ## Making the waiting modifier
+    if(default_branch_length) {
+        modifiers$waiting <- list(fun = branch.length.fast,
+                                    internal = NULL)
+    } else {
+        if(!do_branch_length) {
+            modifiers$waiting <- list(fun = branch.length,
+                                      internal = list(condition = condition,
+                                                      modify    = modify))
         }
+    }
 
-        ## Check object
-        if(test) {
-            check.modifiers(modifiers)
+    ## Making the speciating modifier
+    if(default_speciation) {
+        modifiers$speciating <- list(fun = speciation.fast,
+                                     internal = NULL)
+    } else {
+        if(!do_speciation) {
+            modifiers$speciating <- list(fun = speciation,
+                                         internal = list(condition = condition,
+                                                         modify    = modify))
         }
-        return(modifiers)
     }
 
-    ## Else go to the normal modifiers
-    allowed_modifiers <- c("waiting", "speciating")
-    check.method(what, allowed_modifiers, "what must be")
-
-
-
-    output <- 42
-    class(output) <- c("dads", "modifiers")
-    return(output)
-}
-
-
-waiting.trait.dependent <- function(bd.params, n.taxa, parent.trait, condition = function(x) return(x < 0), modify = function(x) (x * 10)) {
-
-    ## Get the event probability
-    event_probability <- sum(n.taxa * (bd.params$speciation + bd.params$extinction))
-
-    ## Get the waiting time
-    waiting_time <- rexp(1, event_probability)
-
-    ## Modify the waiting time
-    if(condition(parent.trait)) {
-        waiting_time <- modify(waiting_time)
+    if(test) {
+        check.modifiers(modifiers)
     }
 
-    return(waiting_time)
-}
-
-
-speciating.trait.depedent <- function(bd.params, parent.trait, condition = function(x) return(x < 0), modify = function(x) (x * 10)) {
-
-    ## Randomly trigger an event
-    trigger_event <- runif(1)
-
-    ## Modify the triggering
-    if(condition(parent.trait)) {
-        trigger_event <- modify(trigger_event)
-    }
-
-    ## Speciate?
-    return(trigger_event < (bd.params$speciation/(bd.params$speciation + bd.params$extinction)))
+    class(modifiers) <- c("dads", "modifiers")
+    return(modifiers)
 }
