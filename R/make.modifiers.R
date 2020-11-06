f9897bc37a3ab755072094ed3a9ba8c9c47123ff
TGuillerme <guillert@tcd.ie>
2020-11-06 11:56:10 +0000

    sanitizing tested

--- R/make.modifiers.R
+++ R/make.modifiers.R
@@ -4,8 +4,8 @@
 #'
 #' @param branch.length A function for the waiting time generating branch length (see details).
 #' @param speciation    A function for triggering the speciation events (see details).
-#' @param condition     A function giving the condition on which to modify the output of \code{branch.length} or \code{speciation} (see details). If missing the condition is always met (set to \code{condition = function(x, ...) TRUE})
-#' @param modify        A function giving the rule of how to modify the output of \code{branch.length} or \code{speciation} (see details). If missing no modification is used (set to \code{modify = function(x, ...) x}).
+#' @param condition     A function giving the condition on which to modify the output of \code{branch.length} or \code{speciation} (see details). If missing the condition is always met (set to \code{condition = function() TRUE})
+#' @param modify        A function giving the rule of how to modify the output of \code{branch.length} or \code{speciation} (see details). If missing no modification is used (set to \code{modify = function(x) x}).
 #' @param add           Whether to add this modifier to a \code{"dads"} \code{"modifier"} object.
 #' @param test          Logical whether to test if the modifiers object will work (default is TRUE),
 #' @param ...           Optional, any extra argument to be passed to the modifier function.
@@ -46,7 +46,7 @@
 make.modifiers <- function(branch.length, speciation, condition, modify, add, test = TRUE, ...) {
 
     ## required arguments
-    required_args <- c("bd.params", "n.taxa", "parent.lineage", "trait.values", "modify.fun", "...")
+    required_args <- c("bd.params", "n.taxa", "parent.lineage", "trait.values", "modify.fun")
 
     ## Check branch length
     do_branch_length <- FALSE
@@ -54,8 +54,8 @@ make.modifiers <- function(branch.length, speciation, condition, modify, add, te
         check.class(branch.length, "function")
         ## Check if it has the right arguments
         check_args <- names(formals(branch.length))
-        if(any(missing <- is.na(match(required_args, branch_length_args)))) {
-            stop(paste0("The branch.length function is missing the following argument", ifelse(sum(missing) > 1, "s: ", ": "),  paste(required_args[missing], collapse = ", "  ), ". If ", ifelse(sum(missing) > 1, "they are", "it is"), " not required, you can set ", ifelse(sum(missing) > 1, "them", "it"), " to NULL."), call. = FALSE)
+        if(any(missing <- is.na(match(required_args, check_args)))) {
+            stop(paste0("The branch.length function is missing the following argument", ifelse(sum(missing) > 1, "s: ", ": "),  paste(required_args[missing], collapse = ", "), ". If ", ifelse(sum(missing) > 1, "they are", "it is"), " not required, you can set ", ifelse(sum(missing) > 1, "them", "it"), " to NULL."), call. = FALSE)
         }
         do_branch_length <- TRUE
     }
@@ -67,7 +67,7 @@ make.modifiers <- function(branch.length, speciation, condition, modify, add, te
         ## Check if it has the right arguments
         check_args <- names(formals(speciation))
         if(any(missing <- is.na(match(required_args, check_args)))) {
-            stop(paste0("The speciation function is missing the following argument", ifelse(sum(missing) > 1, "s: ", ": "),  paste(required_args[missing], collapse = ", "  ), ". If ", ifelse(sum(missing) > 1, "they are", "it is"), " not required, you can set ", ifelse(sum(missing) > 1, "them", "it"), " to NULL."), call. = FALSE)
+            stop(paste0("The speciation function is missing the following argument", ifelse(sum(missing) > 1, "s: ", ": "),  paste(required_args[missing], collapse = ", "), ". If ", ifelse(sum(missing) > 1, "they are", "it is"), " not required, you can set ", ifelse(sum(missing) > 1, "them", "it"), " to NULL."), call. = FALSE)
         }
         do_speciation <- TRUE
     }
@@ -77,6 +77,14 @@ make.modifiers <- function(branch.length, speciation, condition, modify, add, te
     if(!missing(condition)) {
         check.class(condition, "function")
         do_condition <- TRUE
+        ## Check the arguments
+        check_args <- names(formals(condition))
+        if(any(incorrect <- is.na(match(check_args, required_args)))) {
+            stop(paste0("The condition function cannot recognise the ", paste(check_args[incorrect], collapse = ", "), " argument", ifelse(sum(incorrect) > 1, "s.", ".")), call. = FALSE)
+        }
+    } else {
+        ## Default condition
+        condition <- function() return(TRUE)
     }
 
     ## Check modify
@@ -84,12 +92,22 @@ make.modifiers <- function(branch.length, speciation, condition, modify, add, te
     if(!missing(modify)) {
         check.class(modify, "function")
         do_modify <- TRUE
+        ## Check the arguments
+        check_args <- names(formals(modify))
+        if(!("x" %in% check_args)) {
+            stop(paste0("The modify function must have at least one x argument (you can use x = NULL)."))
+        }
+        if(any(incorrect <- is.na(match(check_args, c("x", required_args))))) {
+            stop(paste0("The modify function cannot recognise the ", paste(check_args[incorrect], collapse = ", "), " argument", ifelse(sum(incorrect) > 1, "s.", ".")), call. = FALSE)
+        }
+    } else {
+        ## Default modify
+        modify <- function(x) return(x)
     }
 
     ## test
     check.class(test, "logical")
     
-
     ## Set the defaults
     default_branch_length <- !do_branch_length && !do_condition && !do_modify
     default_speciation    <- !do_speciation    && !do_condition && !do_modify
@@ -131,7 +149,7 @@ make.modifiers <- function(branch.length, speciation, condition, modify, add, te
         modifiers$waiting <- list(fun = branch.length.fast,
                                     internal = NULL)
     } else {
-        if(!do_branch_length) {
+        if(do_branch_length) {
             modifiers$waiting <- list(fun = branch.length,
                                       internal = list(condition = condition,
                                                       modify    = modify))
@@ -143,7 +161,7 @@ make.modifiers <- function(branch.length, speciation, condition, modify, add, te
         modifiers$speciating <- list(fun = speciation.fast,
                                      internal = NULL)
     } else {
-        if(!do_speciation) {
+        if(do_speciation) {
             modifiers$speciating <- list(fun = speciation,
                                          internal = list(condition = condition,
                                                          modify    = modify))
--- tests/testthat/test-make.modifiers.R
+++ tests/testthat/test-make.modifiers.R
@@ -1,16 +1,55 @@
-# context("make.modifiers")
+context("make.modifiers")
 
-# ## Test
-# test_that("make.modifiers works", {
+## Test
+test_that("make.modifiers works", {
 
-#     ## Sanitizing
-#     expect_error(make.modifiers())
+# branch.length <- branch.length.trait
+# speciation <- speciation.trait
+# condition <- function(trait.values, parent.lineage) return(get.parent.traits(trait.values, parent.lineage) < 0)
+# modify <- function(x) return(x * 10)
+# add = FALSE
+# test = TRUE
 
-#     ## Right output
-#     expect_is(
-#         make.modifiers()
-#         , "class")
-#     expect_equal(
-#         dim(make.modifiers())
-#         , dim)
-# })
+
+    ## Sanitizing
+    ## Wrong arguments for branch length
+    wrong.bl <- function(x, y, n.taxa) return("ah")
+    error <- capture_error(make.modifiers(branch.length = wrong.bl))
+    expect_equal(error[[1]], "The branch.length function is missing the following arguments: bd.params, parent.lineage, trait.values, modify.fun. If they are not required, you can set them to NULL.")
+    wrong.bl <- function(bd.params = NULL, parent.lineage = NULL, trait.values = NULL, modify.fun = NULL) return("ah")
+    error <- capture_error(make.modifiers(branch.length = wrong.bl))
+    expect_equal(error[[1]], "The branch.length function is missing the following argument: n.taxa. If it is not required, you can set it to NULL.")
+
+    ## Wrong arguments for speciation
+    wrong.sp <- function(x, y, n.taxa) return("ah")
+    error <- capture_error(make.modifiers(speciation = wrong.sp))
+    expect_equal(error[[1]], "The speciation function is missing the following arguments: bd.params, parent.lineage, trait.values, modify.fun. If they are not required, you can set them to NULL.")
+    wrong.sp <- function(bd.params = NULL, parent.lineage = NULL, trait.values = NULL, modify.fun = NULL) return("ah")
+    error <- capture_error(make.modifiers(speciation = wrong.sp))
+    expect_equal(error[[1]], "The speciation function is missing the following argument: n.taxa. If it is not required, you can set it to NULL.")
+
+    ## Wrong arguments for condition
+    wrong.con <- function(x) return("a")
+    error <- capture_error(make.modifiers(condition = wrong.con))
+    expect_equal(error[[1]], "The condition function cannot recognise the x argument.")
+    wrong.con <- function(n.taxa, y, ya) return("a")
+    error <- capture_error(make.modifiers(condition = wrong.con))
+    expect_equal(error[[1]], "The condition function cannot recognise the y, ya arguments.")
+
+    # ## Wrong arguments for modify
+    wrong.mod <- function(x, y) return("a")
+    error <- capture_error(make.modifiers(modify = wrong.mod))
+    expect_equal(error[[1]], "The modify function cannot recognise the y argument.")
+    wrong.mod <- function(n.taxa, y) return("a")
+    error <- capture_error(make.modifiers(modify = wrong.mod))
+    expect_equal(error[[1]], "The modify function must have at least one x argument (you can use x = NULL).")
+
+    ## Wrong arguments for add
+    error <- capture_error(make.modifiers(add = TRUE))
+    expect_equal(error[[1]], "modifiers can only be added to objects of class dads and modifiers")
+
+    ## Wrong arguments for test
+    error <- capture_error(make.modifiers(test = "whatever"))
+    expect_equal(error[[1]], "test must be of class logical.")
+
+})
