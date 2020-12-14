e21cf20ee337cf92f0f64f85e021d318d1848833
TGuillerme <guillert@tcd.ie>
2020-12-14 17:40:46 +0000

    updated make.events (still empty)

--- tests/testthat/test-make.events.R
+++ tests/testthat/test-make.events.R
@@ -1,16 +1,8 @@
-# context("make.events")
+## Test
+test_that("make.events works", {
 
-# ## Test
-# test_that("make.events works", {
+    ## Sanitizing
+    #expect_error(make.events())
 
-#     ## Sanitizing
-#     expect_error(make.events())
 
-#     ## Right output
-#     expect_is(
-#         make.events()
-#         , "class")
-#     expect_equal(
-#         dim(make.events())
-#         , dim)
-# })
+})
