d457afcf6bf1165c0f63b0477cc98951702f4e02
TGuillerme <guillert@tcd.ie>
2021-01-07 14:29:36 +0000

    Some tests for make.events

--- tests/testthat/test-make.events.R
+++ tests/testthat/test-make.events.R
@@ -1,27 +1,28 @@
 ## Test
 test_that("make.events works", {
 
-    ## Sanitizing
-    #expect_error(make.events())
+    condition.test.ok <- time.condition(5)
+    condition.test.wrong <- "bob"
+    modification.test.ok <- update.traits(process = OU.process)
+    modification.test.wrong <- "bub"
 
+    error <- capture_error(make.events(target = "target"))
+    expect_equal(error[[1]], "target argument must be one of the following: taxa, bd.params, traits, modifiers, founding.")
 
+    error <- capture_error(make.events(target = "taxa", condition = condition.test.wrong))
+    expect_equal(error[[1]], "function for condition is not a function.")
+
+    error <- capture_error(make.events(target = "bd.params", condition = condition.test.ok, modification = modification.test.wrong))
+    expect_equal(error[[1]], "function for modification is not a function.")
+
+    error <- capture_error(make.events(target = "traits", condition = condition.test.ok, modification = modification.test.ok, additional.args = "bib"))
+    expect_equal(error[[1]], "additional.args must be of class list.")
 })
 
+# test_that("More that one events works", {
 
-## Test
-test_that("condition works", {
-
-    ## Sanitizing
-    #expect_error(make.events())
+#     ## Sanitizing
+#     #expect_error(make.events())
 
 
-})
-
-## Test
-test_that("modification works", {
-
-    ## Sanitizing
-    #expect_error(make.events())
-
-
-})
+# })
