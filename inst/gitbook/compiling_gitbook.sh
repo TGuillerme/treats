f4d70731d71ac9bcbe6f2346236664f8d9593840
Thomas Guillerme <guillert@tcd.ie>
2023-03-27 17:07:19 +0100

    manual big update

--- inst/gitbook/compiling_gitbook.sh
+++ inst/gitbook/compiling_gitbook.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 rm dads_manual.Rmd
 
 ## Remove files before hand
@@ -9,12 +7,6 @@ rm -R _bookdown_files/
 ## Compile the html
 R -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook")' # HTML version
 
-## Comment out the badges
-sed 's/<!-- badges out start -->/<!-- badges out start/g' index.Rmd > index.tmp
-sed 's/<!-- badges out end   -->/badges out end   -->/g' index.tmp > index.Rmd
-rm index.tmp
-
-## Compile the pdf
 R -e 'bookdown::render_book("index.Rmd", "bookdown::pdf_book")' # PDF version
 
 ## Reactivate the badges
