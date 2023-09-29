70256148597674b8b8989a9b78f66a6ecadca2fe
Thomas Guillerme <guillert@tcd.ie>
2023-09-29 12:46:41 +0100

    Debug tools for lineage structure

---
+++ R/topo.convert.lineage.R
@@ -0,0 +1,72 @@
+## FOR DEBUG USE ONLY
+# @title Convert lineage to topology
+#
+# @description Converts a lineage object into a tree object (for visualisation)
+#
+# @param lineage  The lineage object (internal to birth.death_fun.R)
+# @param edge_lengths  Optional, the edge_lengths
+# @param element.names logical, whether to display the elements as named in lineage (TRUE) or as named in the output tree (FALSE)
+# @param plot logical, also plots the tree (TRUE) or not (FALSE)
+topo.convert.lineage <- function(lineage, edge_lengths, element.names = TRUE, plot = TRUE) {
+
+    ## Missing edge lenghts
+    if(missing(edge_lengths)) {
+        edge_lengths <- rep(1, length(lineage$parents))
+    }
+
+    ## Summarise into a table (minus the initiation)
+    table <- data.frame(parent       = lineage$parents, # These are nodes
+                        element      = seq_along(lineage$split), # These are tips or nodes
+                        edge_lengths = edge_lengths,
+                        is_node      = lineage$split)[-1, ]
+
+    n_nodes <- sum(table$is_node)+1
+    n_tips  <- sum(!table$is_node)
+
+    ## Getting the edge table node/tips IDs
+    table$element2 <- NA
+    table$element2[!table$is_node] <- 1:n_tips
+    table$element2[ table$is_node] <- order(table$element[table$is_node]) + n_tips + 1
+
+    ## Getting the edge table nodes (left column)
+    left_edges <- match(table$parent, table$element)
+    table$parent2 <- table$element2[left_edges]
+    ## Add the root (is NA)
+    table$parent2[is.na(table$parent2)] <- n_tips + 1
+
+    ## Getting the tips and nodes labels
+    tip_labels_IDs <- table$element[match(1:n_tips, table$element2)]
+    node_labels_IDs <- c(1, na.omit(table$element[match((n_tips+1):(n_tips+n_nodes), table$element2)]))
+
+    if(!element.names) {
+        tree_tips_labels <- paste0("t", 1:n_tips)
+        tree_node_labels <- paste0("n", 1:n_nodes)
+    } else {
+        tree_tips_labels <- tip_labels_IDs
+        tree_node_labels <- node_labels_IDs
+    }
+
+    tree <- list(edge        = cbind(table$parent2, table$element2),
+                 Nnode       = n_nodes,
+                 tip.label   = tree_tips_labels,
+                 node.label  = tree_node_labels,
+                 edge.length = table$edge_lengths)
+    class(tree) <- "phylo"
+
+    if(plot) {
+        plot(tree)
+        axisPhylo()
+        nodelabels(tree$node.label)
+        ## Highlight the current
+        current <- rep(NA, n_tips)
+        current[which(tip_labels_IDs == lineage$current)] <- 0
+        tiplabels(pch = current, bg = NULL, col = "black", cex = 2.2, adj = 0.9)
+        ## Highlight the livings
+        livings <- rep(NA, n_tips)
+        livings[tip_labels_IDs %in% lineage$livings] <- 1
+        tiplabels(pch = livings, bg = NULL, col = "black", cex = 2.5, adj = 0.9)
+        ## Legend
+        legend("bottomleft", pch = c(0,1), legend = c("current", "living"))
+    }
+    return(tree)
+}
---
+++ lineage_object_structure.md
@@ -0,0 +1,21 @@
+### Structure of the internal `lineage` list  (for developers)
+
+lineage IDs here represent the element's number (tip or node). It get assigned by order of appearance starting with 0, the original root (leading to 1, the actual root of the tree).
+
+```
+lineage
+    |
+    \---$parents = an "integer" vector: the list of parent lineages;
+    |
+    \---$livings = n "integer" vector: the list of lineages still not extinct;
+    |   
+    \---$drawn = a single "integer": the ID of the selected lineage; this must be a number in 1:lineage$n;
+    |
+    \---$current = a single "integer": the selected lineage (is equal to lineage$livings[lineage$drawn]);
+    |
+    \---$n = a single "integer": the current number of non extinct lineage (is equal to length(lineage$livings));
+    |
+    \---$split = a "logical" vector: the list of splits for each lineage (TRUE), the number of total tips is equal to sum(!lineage$split).
+```
+
+You can internally visiualise a lineage object by loading the package via `devtools::load_all()` and use `topo.convert.lineage(lineage)`.
