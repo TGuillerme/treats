3c41655d01ede285224167d92d80c01ac717b44c
Thomas Guillerme <guillert@tcd.ie>
2023-10-16 13:37:54 +0100

    Up response

--- tests/test-benchmarking.R
+++ tests/test-benchmarking.R
@@ -1,69 +1,86 @@
 # ## Benchmarking with RPANDA and TreeSim
 
-# library(TreeSim)
+library(TreeSim)
+library(diversitree)
+library(RPANDA)
+
+## Function for repeating the simulation until a result appears
+safe.sim <- function(fun, args) {
+    out <- list(0)
+    null.error <- 0
+    while(!is(out[[1]], "phylo") && null.error < 100) {
+        cat(".")
+        out <- do.call(fun, args)
+        null.error <- null.error + 1
+    }
+    return(out)
+}
+
+sim_ClaDS_tree <- function(...) {
+    return(sim_ClaDS(...)$tree)
+}
+
+###########
+# Testing simulating a tree
+###########
+
+set.seed(1)
+## Testing parameters
+age <- 3
+lambda <- 2.0
+mu <- 0.5
+numbsim <- 1
+
+params_sim.bd.age <- list(age = age, lambda = lambda, mu = mu, numbsim = 1, mrca = FALSE)
+params_tree.bd <- list(pars = c(lambda, mu), max.taxa = Inf, max.t = age, include.extinct = TRUE)
+params_sim_ClaDS <- list(lambda_0 = lambda, mu_0 = mu, condition = "time", time_stop = age, prune_extinct = FALSE)
+params_treats <- list(stop.rule = list(max.time = age), bd.params = list(speciation = lambda, extinction = mu), null.error = 100, verbose = FALSE)
+
+test_age <- microbenchmark(times = 5L,
+    TreeSim = safe.sim(sim.bd.age, params_sim.bd.age),
+    diversitree = safe.sim(tree.bd, params_tree.bd),
+    RPANDA = safe.sim(sim_ClaDS_tree, params_sim_ClaDS),
+    treats = safe.sim(treats, params_treats)
+)
 
 
-# ## Simulate 3 birth death tree to reach age = 2 
-# age<-3
-# lambda <- 2.0
-# mu <- 0.5
-# frac <-0.6
-# numbsim<-20
-# ##
-# # Simulating trees with time age since the origin:
-# safe.sim <- function(fun, args) {
-#     out <- list(0)
-#     null.error <- 0
-#     while(!is(out[[1]], "phylo") && null.error < 50) {
-#         out <- do.call(fun, args)
-#         null.error <- null.error + 1
-#     }
-#     return(out)
-# }
-
-# set.seed(1)
-
-# test_age_2_fail <- microbenchmark(
-#     treeSim = sim.bd.age(age = 2, lambda = lambda, mu = mu, numbsim = 1),
-#     treats = treats(stop.rule = list(max.time = 2), bd.params = list(speciation = lambda, extinction = mu), null.error = TRUE))
-# test_age_3_fail <- microbenchmark(
-#     treeSim = sim.bd.age(age = 3, lambda = lambda, mu = mu, numbsim = 1),
-#     treats = treats(stop.rule = list(max.time = 3), bd.params = list(speciation = lambda, extinction = mu), null.error = TRUE))
-# test_age_4_fail <- microbenchmark(
-#     treeSim = sim.bd.age(age = 4, lambda = lambda, mu = mu, numbsim = 1),
-#     treats = treats(stop.rule = list(max.time = 4), bd.params = list(speciation = lambda, extinction = mu), null.error = TRUE))
-# test_age_5_fail <- microbenchmark(
-#     treeSim = sim.bd.age(age = 5, lambda = lambda, mu = mu, numbsim = 1),
-#     treats = treats(stop.rule = list(max.time = 5), bd.params = list(speciation = lambda, extinction = mu), null.error = TRUE))
-
-# n<-10
-# lambda <- 2.0
-# mu <- 0.5
-# frac <-0.6
-# numbsim<-1
-# ##
-# # Simulating numbsim trees with n species under a birth-death process with
-# # speciation rate lambda an extinction rate mu:
-# sim.bd.taxa(n,numbsim,lambda,mu)
 
 
-# test_taxa_10_fail <- microbenchmark(
-#     treeSim = sim.bd.taxa(n = 10, lambda = lambda, mu = mu, numbsim = 1),
-#     treats = treats(stop.rule = list(max.living = 10), bd.params = list(speciation = lambda, extinction = mu), null.error = TRUE))
-# test_taxa_50_fail <- microbenchmark(
-#     treeSim = sim.bd.taxa(n = 50, lambda = lambda, mu = mu, numbsim = 1),
-#     treats = treats(stop.rule = list(max.living = 50), bd.params = list(speciation = lambda, extinction = mu), null.error = TRUE))
-# test_taxa_150_fail <- microbenchmark(
-#     treeSim = sim.bd.taxa(n = 150, lambda = lambda, mu = mu, numbsim = 1),
-#     treats = treats(stop.rule = list(max.living = 150), bd.params = list(speciation = lambda, extinction = mu), null.error = TRUE))
-# test_taxa_500_fail <- microbenchmark(
-#     treeSim = sim.bd.taxa(n = 500, lambda = lambda, mu = mu, numbsim = 1),
-#     treats = treats(stop.rule = list(max.living = 500), bd.params = list(speciation = lambda, extinction = mu), null.error = TRUE))
+tree.bd(pars, max.taxa=Inf, max.t=time, include.extinct=FALSE)
+
+###########
+# Testing simulating a tree with an extinction event
+###########
 
 
-# ## On average, TreeSim is 2-3x faster
+###########
+# Testing simulating a tree and traits
+###########
 
 
+
+###########
+# Testing simulating a tree and traits with extinction event
+###########
+
+
+
+
+plot(test_age, ylab = "microseconds", xlab = "")
+
+
+
+# Simulating trees with a fixed number of species having shifts in rate
+# and mass extinction events.
+# Between today and time 0.3 in the past, we have speciation rate 2,
+# extinction rate 0. At time 0.3, we have a mass extinction event which
+# 10% of the species survive. Prior to 0.3, we have a speciation rate
+# of 1 and an extinction rate of 0.3:
+sim.rateshift.taxa(n,numbsim,c(2,1),c(0,0.3), c(1,0.1),c(0,0.3),complete=TRUE)
+# The fraction 0.6 of the extant species is included into the final tree
+# (the tree has n species AFTER sampling, extinct and
+# non-sampled lineages are not included):
+sim.rateshift.taxa(n,numbsim,c(2,1),c(0,0.3), c(0.6,0.1),c(0,0.3),complete=FALSE)
 
 
 
@@ -76,21 +93,6 @@
 # # 10% of the species survive. Prior to 0.3, we have a speciation rate
 # # of 1 and an extinction rate of 0.3:
 # sim.rateshift.taxa(n,numbsim, c(2,1), c(0,0.3), c(1,0.1), c(0,0.3),complete=TRUE)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
