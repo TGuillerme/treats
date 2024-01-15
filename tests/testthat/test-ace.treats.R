ee0be5b77052d4f1aaa028bec5de4ad8633f0643
Thomas Guillerme <guillert@tcd.ie>
2024-01-15 15:35:07 +0000

    basic (not good!) ace

---
+++ tests/testthat/test-ace.treats.R
@@ -0,0 +1,16 @@
+test_that("ace.treats implementation works", {
+
+    set.seed(1)
+    data <- treats(stop.rule = list(max.living = 40),
+                   trait = make.traits(),
+                   bd.params = list(speciation = 1, exctinction = 0.2),
+                   null.error = 10)
+    ## Remove the node values
+    data$data <- data$data[-c(grep("n", rownames(data$data))), , drop = FALSE]
+
+    ## Run some basic estimations
+    test <- ace.treats(treats = data, traits = make.traits(), replicates = 5)
+
+    plot(test)
+
+})
