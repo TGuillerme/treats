8eb26bcead630ac9f1632f0eea25cec3050aceb9
calebowski <cnscutt1@sheffield.ac.uk>
2025-07-16 14:40:17 +0100

    trait.extinction works for multiple trait selectivity

--- R/events.modifications.R
+++ R/events.modifications.R
@@ -183,20 +183,46 @@ random.extinction <- function(x){
 }
 
 ## Mass extinction based on traits modification
-trait.extinction.multi <- function(x, condition = `<`, traits = c(1, 3, 6)) {
+trait.extinction <- function(x, condition = `<`, trait = 1, intensity) {
+    
+    ## Function for extinction trait
     extinction.trait <- function(bd.params, lineage, trait.values) {
+        
+        ## Select the nodes by traits
         parent_traits <- parent.traits(trait.values, lineage, current = FALSE)
-        trait_checks <- sapply(traits, function(trait_idx) {
-            condition(parent_traits[, trait_idx], x)
-        })
-        selected_nodes <- as.numeric(names(which(apply(trait_checks, 1, all))))
-        extinct <- which(lineage$parents %in% selected_nodes)
+        
+        ## Check if we have enough traits
+        if(ncol(parent_traits) < max(trait)) {
+            return(lineage)  # Return unchanged if not enough traits
+        }
+        
+        ## Handle multiple traits with AND condition
+        if(length(trait) == 1) {
+            # Single trait (original behavior)
+            selected_nodes <- as.numeric(names(which(condition(parent_traits[, trait], x))))
+        } else {
+            # Multiple traits - ALL must meet the condition
+            trait_checks <- sapply(trait, function(trait_idx) {
+                condition(parent_traits[, trait_idx], x)
+            })
+            # Use apply with 'all' to ensure ALL traits meet the condition
+            selected_nodes <- as.numeric(names(which(apply(trait_checks, 1, any))))
+        }
+        
+        ## Select the descendants that'll go extinct
+        selected <- which(lineage$parents %in% selected_nodes)
+        extinct <- sample(selected, size = ceiling(length(selected) * intensity))
+
+        
+        ## Update the lineage object
         lineage$livings <- lineage$livings[!lineage$livings %in% extinct]
-        lineage$n <- length(lineage$livings)
+        lineage$n       <- length(lineage$livings)
+        
         return(lineage)
     }
     return(extinction.trait)
 }
+
 
 ## Updating the bd.params
 bd.params.update <- function(x, speciation = NULL, extinction = NULL, joint = NULL, absolute = NULL, speciation.args = NULL, extinction.args = NULL) {
